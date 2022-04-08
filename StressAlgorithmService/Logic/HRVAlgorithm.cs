32193dde44ca9ab447be1c5c662c49b5118b9f89
Klein,Bas B.C <bas.klein@student.fontys.nl>
2022-04-08 14:40:15 +0200

    Made the first HRV algorithm, and added a test project with tests that are working

--- StressAlgorithmService/Logic/HRVAlgorithm.cs
+++ StressAlgorithmService/Logic/HRVAlgorithm.cs
@@ -7,7 +7,42 @@ namespace StressAlgorithmService.Logic
     {
         public List<HeartRateMeasurement> CalculateHRV(List<HeartRateMeasurement> heartRates)
         {
+            int[] intervals = (from HeartRateMeasurement heartRate in heartRates
+                              select heartRate.HeartRateInterval).ToArray();
+            double hrv = CalculateHRVBasedOnIntervals(intervals);
             throw new NotImplementedException();
+        }
+        
+        public int CalculateHRVBasedOnIntervals(int[] intervals)
+        {
+            int lastInterval = 0;
+            List<int> intervalDifferences = new();
+            foreach (int interval in intervals)
+            {
+                // Skip the first interval
+                if(lastInterval != 0)
+                {
+                    intervalDifferences.Add(DifferenceBetweenIntervalsToPower2(lastInterval, interval));
+                }
+                lastInterval = interval;
+            }
+            double average = FindAverage(intervalDifferences.ToArray());
+            return (int)Math.Round(Math.Sqrt(average));
+        }
+        //Calculates the difference between 2 integer values, and does it to the power 2
+        public int DifferenceBetweenIntervalsToPower2(int interval1, int interval2)
+        {
+            return (int)Math.Pow(interval1 - interval2, 2);
+        }
+        //calculates the avarage of a list of integers
+        public double FindAverage(int[] intervalDifferences)
+        {
+            int total = 0;
+            foreach(int interval in intervalDifferences)
+            {
+                total += interval;
+            }
+            return total/intervalDifferences.Length;
         }
     }
 }
---
+++ StressAlgorithmServiceTests/HRVAlgorithmTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StressAlgorithmService.Logic;
+
+namespace StressAlgorithmService.Tests
+{
+    [TestClass]
+    public class HRVAlgorithmTests
+    {
+        HRVAlgorithm algorithm = new HRVAlgorithm();
+        [DataTestMethod]
+        [DataRow(744, 427, 100489)]
+        [DataRow(427, 498, 5041)]
+        [DataRow(498, 931, 187489)]
+        public void IntervalTest(int firstInterval, int secondInterval, int expected)
+        {
+            int actual = algorithm.DifferenceBetweenIntervalsToPower2(firstInterval, secondInterval);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AverageTest()
+        {
+            double expected = 97673;
+            int[] intervals = new int[] { 100489, 5041, 187489 };
+
+            double actual = algorithm.FindAverage(intervals);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void testCalculateHRVByInterval()
+        {
+            int expected = 185;
+            int[] intervals = new int[] { 1000, 950, 1000, 860, 1230, 1020, 1000 };
+            int hrv = algorithm.CalculateHRVBasedOnIntervals(intervals);
+            Assert.AreNotEqual(hrv, 0);
+        }
+        [TestMethod]
+        public void testCalculateHRVByInterval0()
+        {
+            int expected = 0;
+            int[] intervals = new int[] { 1000, 1000, 1000, 1000, 1000 };
+            int hrv = algorithm.CalculateHRVBasedOnIntervals(intervals);
+            Assert.AreEqual(hrv, expected);
+        }
+        [TestMethod]
+        public void testCalculateHRVByInterval200()
+        {
+            int expected = 200;
+            int[] intervals = new int[] { 1000, 800, 1000, 800, 1000 };
+            int hrv = algorithm.CalculateHRVBasedOnIntervals(intervals);
+            Assert.AreEqual(hrv, expected);
+        }
+    }
+}
